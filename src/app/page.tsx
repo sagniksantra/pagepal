5c66ba40f9706f69cfc67332ac439cd30ab551c9
Sagnik <santrasagnik10@gmail.com>
2023-09-16 12:30:02 +0530

    added Auth

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,8 @@
+import { Button } from '@/components/ui/button'
 import Image from 'next/image'
 
 export default function Home() {
-  return (
-    <h1>Hello</h1>
+  return( 
+    <Button>Hello</Button>
   )
 }
