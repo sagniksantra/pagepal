73eb2258faf5ae36f11b760c7051d36e86982015
Sagnik <santrasagnik10@gmail.com>
2023-09-18 04:32:06 +0530

    setup React query

--- src/components/FileUpload.tsx
+++ src/components/FileUpload.tsx
@@ -4,25 +4,28 @@ import { Inbox, Loader2 } from "lucide-react";
 import React from "react";
 import { useDropzone } from "react-dropzone";
 import toast, { Toaster } from 'react-hot-toast';
+import { useMutation } from "@tanstack/react-query";
+import axios from "axios";
+import { useRouter } from "next/navigation";
 
 const FileUpload = () => {
 //   const router = useRouter();
-//   const [uploading, setUploading] = React.useState(false);
-//   const { mutate, isLoading } = useMutation({
-//     mutationFn: async ({
-//       file_key,
-//       file_name,
-//     }: {
-//       file_key: string;
-//       file_name: string;
-//     }) => {
-//       const response = await axios.post("/api/create-chat", {
-//         file_key,
-//         file_name,
-//       });
-//       return response.data;
-//     },
-//   });
+  const [uploading, setUploading] = React.useState(false);
+  const { mutate, isLoading } = useMutation({
+    mutationFn: async ({
+      file_key,
+      file_name,
+    }: {
+      file_key: string;
+      file_name: string;
+    }) => {
+      const response = await axios.post("/api/create-chat", {
+        file_key,
+        file_name,
+      });
+      return response.data;
+    },
+  });
 
   const { getRootProps, getInputProps } = useDropzone({
     accept: { "application/pdf": [".pdf"] },
@@ -32,36 +35,42 @@ const FileUpload = () => {
       const file = acceptedFiles[0];
       if (file.size > 10 * 1024 * 1024) {
         // bigger than 10mb!
-        // toast.error("File too large");
+        toast.error("File too large");
         alert('bigger than 10mb!')
         return;
       }
 
       try {
-        // setUploading(true);
+        setUploading(true);
         const data = await uploadToS3(file);
         console.log("data", data)
-        // if (!data?.file_key || !data.file_name) {
-        //   toast.error("Something went wrong");
-        //   return;
-        // }
-        // mutate(data, {
-        //   onSuccess: ({ chat_id }) => {
-        //     toast.success("Chat created!");
-        //     router.push(`/chat/${chat_id}`);
-        //   },
-        //   onError: (err) => {
-        //     toast.error("Error creating chat");
-        //     console.error(err);
-        //   },
-        // });
+        if (!data?.file_key || !data.file_name) {
+          toast.error("Something went wrong");
+          return;
+        }
+        mutate(data, {
+          // onSuccess: ({ chat_id }) => {
+          //   toast.success("Chat created!");
+          //   // router.push(`/chat/${chat_id}`);
+          // },
+          onSuccess: (data) => {
+            console.log("data", data);
+            toast.success(data.message);
+          },
+          onError: (err) => {
+            toast.error("Error creating chat");
+            console.error(err);
+          },
+        });
       } catch (error) {
         console.log(error);
-    //   } finally {
-    //     setUploading(false);
+      } finally {
+        setUploading(false);
       }
     },
   });
+
+
   return (
     <div className="p-2 bg-white rounded-xl">
       <div
@@ -71,10 +80,20 @@ const FileUpload = () => {
         })}
       >
         <input {...getInputProps()} />
+        {uploading || isLoading ? (
+          <>
+            {/* loading state */}
+            <Loader2 className="h-10 w-10 text-blue-500 animate-spin" />
+            <p className="mt-2 text-sm text-slate-400">
+              Spilling Tea to GPT...
+            </p>
+          </>
+        ) : (
           <>
             <Inbox className="w-10 h-10 text-blue-500" />
             <p className="mt-2 text-sm text-slate-400">Drop PDF Here</p>
           </>
+        )}
       </div>
     </div>
   );
